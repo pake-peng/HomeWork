e9a511ae2ba77bb0cc03f3246eb4d104417a8f9e
pake-peng <43206082+pake-peng@users.noreply.github.com>
2018-09-21 15:31:51 +0800

    Add files via upload

---
+++ 猜数字游戏作业src/GameBase.java
@@ -0,0 +1,128 @@
+
+/**
+ * 
+ * @author 彭湃
+ * @version java 10
+ * 
+ * 属性：
+ * 		用户得分，随机数
+ * 方法:
+ * 		界面
+ * 		随机数的重置
+ * 		得分累计
+ *		随机数的检测
+ */
+import java.lang.*;
+import java.util.Scanner;
+
+public class GameBase 
+{
+	private int score;
+	private int randomNumber;
+	private int diffcult;
+	
+	public GameBase()
+	{
+		score = 6;
+		randomNumber = -1;
+		diffcult = 1;
+	}
+	
+	//设置随机数函数
+	public void setRandomNumber()
+	{
+		randomNumber = (int)(Math.random() * (diffcult * 10)) + 1;
+	}
+	
+	
+	public int getRandomNumber()
+	{
+		return randomNumber;
+	}
+	
+	//菜单打印
+	public void printMenu()
+	{
+		System.out.println("********* Wellcom to paly Guess Game ********* ");
+		System.out.println("********* 1 : crate new game *********");
+		System.out.println("********* 2 : game help *********");
+		System.out.println("********* 3 : set diffcult *********");
+		Scanner in  = new Scanner(System.in);
+		
+		int m = in.nextInt();
+		
+		
+		//检测2，3情况,弹出菜单情况，2，3后递归调用一次该函数用来打印菜单，
+		//如果选中其他数字，则显示输入错误，并重新调用菜单函数 ,直到选中1开始游戏
+		switch(m)
+		{
+		case 1:
+			break;
+		case 2:
+			System.out.println("the Ruler is : choose the correct number ");
+			System.out.println("the game diffluct have list 1 : 1~10, 2 : 2~20, 3 : 1~30， 4 : 1~40");
+			System.out.println(" diffcult 1: loss 1, score 2 : loss 2, 3 : loss 3, 4: loss 4");
+			printMenu();
+			break;
+		case 3:
+			System.out.println("the diffluct you can set is  1 : 1~10, 2 : 2~20, 3 : 1~30， 4 : 1~40");
+			int dTest = in.nextInt();
+			
+			//难度正确输入检测
+			while((dTest < 1) || (dTest > 4))
+			{
+				System.out.println("1 to 4");
+				dTest = in.nextInt();
+			}
+			diffcult = dTest;
+			printMenu();
+			break;
+		default:
+			System.out.println("please enter the number : 1 to 3");
+			printMenu();
+			break;
+		}
+		
+		//in.close();
+		
+	}
+	
+	//扣分机制
+	public boolean lossScore()
+	{
+		if(score < 0)
+			return false;
+		
+		score -= 1 * diffcult;
+		return true;
+	}
+
+
+	
+	//充钱函数
+	public void setScore(int m)
+	{
+		if(m>1000)
+		{
+			System.out.println("you are so greedy");
+			score = 0;	
+		}
+		else if(m < 0)
+		{
+			System.out.println("Are you kiding me,make you dream come true");
+			score = m;
+		}
+		else
+			score = m;
+	}
+	
+	//返回难度函数
+	public int getDiffcult()
+	{
+		return diffcult;
+	}
+	
+	
+	
+
+}
---
+++ 猜数字游戏作业src/Player.java
@@ -0,0 +1,127 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author 彭湃
+ * 属性：
+ * 		姓名
+ * 		游戏对象
+ * 方法:
+ *		开始游戏
+ *		继续游戏
+ *		显示得分
+ *		退出游戏
+ *		
+ */
+
+public class Player 
+{
+	private String name;
+	private GameBase gb;
+	private boolean continueGame;
+	public Player()
+	{
+		gb = new GameBase();
+		continueGame = true;
+	}
+	
+	//开始游戏函数
+	public void StartGame()
+	{
+		Scanner inpute = new Scanner(System.in);
+		
+		//第一次进入游戏弹出的信息输入
+		if (continueGame == true)
+		{
+			gb.printMenu();
+			System.out.println("*** please enter you name ***");
+			setName(inpute.next());
+			System.out.println("@" + name + " welcom to Peng Pai game , good luck to you" );
+			System.out.println("you orginal score is 6  come on !!!!" );
+		}
+		
+		gb.setRandomNumber();
+		//用来记录系统产生的随机数
+		int n = gb.getRandomNumber(); 
+		boolean testScore =true;
+		
+		//循环检测判断数值是否正确
+		while(testScore && true )
+		{
+			System.out.println("please enter you guess Number");
+			int Gnumber = inpute.nextInt();
+			if( Gnumber == n)
+				break;
+			System.out.println("it is pity that " +  Gnumber + " is not correct number");
+			System.out.println("you have lost " + gb.getDiffcult() * 1 + " score!");
+			testScore = gb.lossScore();
+		}
+		
+		//积分不够的处理方式
+		if(testScore == false)
+		{
+			System.out.println("the score less than 0");
+			System.out.println("Do you want to revenge,1: please give money or 2 : get out of game");
+			int r1 = inpute.nextInt();
+				switch(r1)
+				{
+				case 1: 
+					System.out.println("enter the money number");
+					gb.setScore(inpute.nextInt());
+					System.out.println("thank to peng pai!, he had paid for you!!!!!");
+					break;
+				case 2:
+					System.exit(-1);
+				default:
+					System.out.println("there is no choice ,unless you are vip, game over!");
+					System.exit(-1);
+					break;
+				}
+		}
+		
+		else
+			System.out.println("*******恭喜你获得胜利 ，你获得" + gb.getDiffcult() * 20 + "积分    ******* ");
+		
+		System.out.println("Do you want to come on Y/N");
+		String s = inpute.next();
+		
+		//检测是否继续游戏
+		while(true)
+		{
+			if(s.equals("Y"))
+			{
+				continueGame = false;
+				System.out.println("this game is so attracted for you yeah? e"
+						+ "njoy the game absolutely");
+				StartGame();
+				
+				break;
+			}
+			if(s.equals("N"))
+			{
+				System.exit(-1);
+				break;
+			}
+			System.out.println("cheak you char");
+		}
+		
+		inpute.close();
+	}
+	
+	public static void main(String[] args)
+	{
+		Player p1 = new Player();
+		p1.StartGame();
+	}
+	
+	public void setName(String s)
+	{
+		name = s;
+	}
+	
+	public void setContinueGame()
+	{
+		continueGame = false;
+	}
+	
+}
